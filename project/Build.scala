297a0172647e4d8bc357acd6c556611d0614aeb3
Alex Cruise <acruise@splunk.com>
2016-11-12 12:12:31 -0800

    Enable --verbose --show-diff by default. Fixes #1265.
    New synthetic --quiet and --no-diff args are added (only in the sbt build, not in partest itself) to suppress the new defaults.

--- project/Build.scala
+++ project/Build.scala
@@ -126,7 +126,10 @@ object DottyBuild extends Build {
         // Magic! This is both an input task and a dynamic task. Apparently
         // command line arguments get passed to the last task in an aliased
         // sequence (see partest alias below), so this works.
-        val args = Def.spaceDelimited("<arg>").parsed
+        var args = Def.spaceDelimited("<arg>").parsed // var is OK as a local IMO, it makes the following cleaner
+        if (!args.contains("--verbose")   && !args.contains("--quiet"))   args :+= "--verbose"    // note --quiet is invented here, just to suppress the new default
+        if (!args.contains("--show-diff") && !args.contains("--no-diff")) args :+= "--show-diff"  // note --no-diff is invented here, just to suppress the new default
+
         val jars = Seq((packageBin in Compile).value.getAbsolutePath) ++
             getJarPaths(partestDeps.value, ivyPaths.value.ivyHome)
         val dottyJars  = "-dottyJars " + (jars.length + 1) + " dotty.jar" + " " + jars.mkString(" ")
